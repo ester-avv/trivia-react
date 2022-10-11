a9a2dce7acc22953d3dd256e632cd21e338aaa04
rromulo <rromulo.silva21@gmail.com>
2022-10-11 14:55:55 -0300

    conclui requisito 8, validando o tempo e a propriedade disabled dos botoes

---
+++ src/redux/reducers/logicPoints.js
@@ -0,0 +1,15 @@
+import { GET_TIMER } from '../actions/index';
+
+const INITIAL_STATE = {
+  timer: 30,
+  points: 0,
+};
+const logicPoints = (store = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case GET_TIMER:
+    return { ...store, timer: action.payload };
+  default:
+    return store;
+  }
+};
+export default logicPoints;
